9c51182b5df41704ca0e5ee749dd3bbf6101a62d
Diablo <hanon.mickael59@gmail.com>
2024-09-22 12:30:52 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Services from "./pages/Services";
+import Realisations from "./pages/Realisations";
+import Blog from "./pages/Blog";
+import Contact from "./pages/Contact";
+import Legal from "./pages/Legal";
+import ApiGitHub from "./pages/ApiGitHub";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header id="top" />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/realisations" element={<Realisations />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/legal" element={<Legal />} />
+        <Route path="/profil-github" element={<ApiGitHub />} />
+      </Routes>
+
+      <Footer />
     </div>
   );
 }
